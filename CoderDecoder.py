049c1249ba6d9f1de49aeb645610367aa603e462
Jakub Oszust <35966778+kubatheklamca@users.noreply.github.com>
2019-04-15 03:11:46 +0200

    Add files via upload
    dodane kodowanie hamminga (nie dziala na 100%, bo nie sprawdza poprawnosci bitow parzystosci)

--- CoderDecoder.py
+++ CoderDecoder.py
@@ -11,7 +11,49 @@ class Coder:
             packet+='1'
         return packet
 
+    def calcNumberOfRedundantBits(self, packet):
+        r=0
+        while 2**r < len(packet) + r + 1:
+            r+=1
+        return r
+
+    def codeHamming(self, packet):
+        numberOfRedundantBits = self.calcNumberOfRedundantBits(packet)
+        bitpositions = []
+        for bit in range(0,numberOfRedundantBits):
+            bitpos = 2**bit - 1
+            bitpositions.append(bitpos)
+            packet = packet[:bitpos] + ' ' + packet[(bitpos+1):]
+        return self.setParrityBitValues(packet,bitpositions)
+
+    def setParrityBitValues(self, packet, bitpositions):
+        #helper do poteg
+        helper = 0
+        for bit in bitpositions:
+            xor = 0
+            for i in range(1, len(packet)+1):
+                if (i & (1<<(helper))):
+                    if (packet[i-1] != ' '):
+                        xor^=int(packet[i-1])
+            if (xor == 0):
+                packet = packet[:bit] + '0' + packet[(bit+1):]
+            if (xor == 1):
+                packet = packet[:bit] + '1' + packet[(bit+1):]
+            helper+=1
+        return packet
+                
+
 class Decoder:
+    def getReduntantbits(self, packet):
+        r=0
+        reduntantbits = []
+        x = 2**r
+        while x <= len(packet) + r + 1:
+            r+=1
+            reduntantbits.append(x)
+            x = 2**r
+        return reduntantbits
+
     def decodeParrity(self, packet):
         xor = 0
         for i in packet[:-1]:
@@ -20,4 +62,36 @@ class Decoder:
             return True
         else:
             return False
+    
+    def decodeHamming(self, packet):
+        reduntantbits = self.getReduntantbits(packet)
+        self.correctCode(packet,reduntantbits)
+
+    def correctCode(self, packet, reduntantbits):
+        #To samo co w setParrityBitValues- redundacja, mozna zrefaktoryzowac
+        helper = 0
+        failedBit = 0
+        zeroiswrong = False
+        for bit in reduntantbits:
+            xor = 0
+            for i in range(1, len(packet)+1):
+                if (i & (1<<(helper))):
+                    if (packet[i-1] != helper):
+                        xor^=int(packet[i-1])
+            if (xor != int(packet[bit])):
+                if bit == 0:
+                    zeroiswrong = True
+                failedBit+=bit
+            helper+=1
+        # if (zeroiswrong):
+        #     if (int(packet[0]) == 0):
+        #         packet[0] = '1'
+        #     else:
+        #         packet[0] = '0'
+        if (failedBit != 0):
+            if (int(packet[failedBit]) == 0):
+                packet = packet[:failedBit] + '1' + packet[(failedBit+1): ]
+            else:
+                packet[failedBit] = packet[:failedBit] + '0' + packet[(failedBit+1):]
+
 
--- start.py
+++ start.py
@@ -9,14 +9,21 @@ decoder = Decoder()
 sin = datagen.create_sin()
 sin = datagen.convert_y_to_bin(sin) 
 sin = datagen.convert_y_from_bin(sin)
-datagen.show_data(sin)
-sin = datagen.convert_y_to_bin(sin) 
-for i, item in enumerate(sin):  
-    item[1] = coder.codeParrity(item[1])
-    item[1] = channel.distort_packet(item[1], 50)
-    if(not decoder.decodeParrity(item[1])):
+# datagen.show_data(sin)
+sin = datagen.convert_y_to_bin(sin)
+#kod parzystosci, przeslanie jezeli bit sie nie zgadza to odeslanie pakietu 
+for i in range(len(sin)):
+    sin[i][1] = coder.codeParrity(sin[i][1])
+    sin[i][1] = channel.distort_packet(sin[i][1], 4)
+    if(not decoder.decodeParrity(sin[i][1])):
         i-=1
         continue
+#kod hamminga, jezeli jakis bit sie nie zgadza korekcja pakietu, error_rate ustawiony na 4% zeby
+#zostal zepsuty tylko 1 bit
+for i in sin:
+        i[1] = coder.codeHamming(i[1])
+        i[1] = channel.distort_packet(i[1], 4)
+        i[1] = decoder.decodeHamming(i[1])
 sin = datagen.convert_y_from_bin(sin)
 datagen.show_data(sin)
 
